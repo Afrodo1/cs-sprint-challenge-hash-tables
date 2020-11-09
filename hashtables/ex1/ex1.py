4c48d968561637a1fbc390a075c30c2348f348d2
Dominic Bridgette <54685761+Afrodo1@users.noreply.github.com>
2020-11-08 21:37:03 -0800

    not quite mvp still need to do one more ex file

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,28 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    hash = {}
+    lis = [None]
+    if len(weights) >= 2:
+        for x in weights:
+            for y in weights:
+                hash.update({(x,y):x+y})
+        for x in hash:        
+            if hash[x] is limit:
+                lis = [x[0],x[1]]
+                if lis[0] < lis[1]:
+                    lis[0],lis[1] = lis[1],lis[0]
+        for x in range(length):
+            if lis[0] is weights[x]:
+                lis[0] = x
+            if lis[1] is weights[x]:
+                lis[1] = x
+        return lis
+    
 
     return None
+
+    
+weights = [12, 6, 7, 14, 19, 3, 0, 25, 40]
+
+print(get_indices_of_item_weights(weights,9,7))
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,34 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    dest = {}
+    dest2 = ['NONE']
+    count = 0
+    for x in tickets:
+        dest.update({x.source:x.destination})
+        if x.source is "NONE":
+            dest2[0] = x.destination
+    while len(dest2) < length - 1:        
+            for x in dest:                     
+                if x is dest2[count]:
+                    dest2.append(dest[x])
+                    count +=1
+    print(dest2)
 
-    return route
+    return dest2
+
+
+ticket_1 = Ticket("PIT", "ORD")
+ticket_2 = Ticket("XNA", "SAP")
+ticket_3 = Ticket("SFO", "BHM")
+ticket_4 = Ticket("FLG", "XNA")
+ticket_5 = Ticket("NONE", "LAX")
+ticket_6 = Ticket("LAX", "SFO")
+ticket_7 = Ticket("SAP", "SLC")
+ticket_8 = Ticket("ORD", "NONE")
+ticket_9 = Ticket("SLC", "PIT")
+ticket_10 = Ticket("BHM", "FLG")
+tickets = [ticket_1, ticket_2, ticket_3, ticket_4, ticket_5,
+                   ticket_6, ticket_7, ticket_8, ticket_9, ticket_10]
+            
+reconstruct_trip(tickets,10)
